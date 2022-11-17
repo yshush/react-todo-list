52c0b0676090514b3e8b61b7b84541466e7f83b5
양성훈 <tjdgns7533@gmail.com>
2022-11-18 08:27:58 +0900

    style-header

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ const filters = ["all", "active", "completed"];
 function App() {
   const [filter, setFilter] = useState(filters[0]);
   return (
-    <div>
+    <>
       <Header filters={filters} filter={filter} onFilterChange={setFilter} />
       <TodoList filter={filter} />
-    </div>
+    </>
   );
 }
 
